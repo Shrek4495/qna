c74ce242f1c19a3b4a9a426c5e878338b31875d7
shrek4495 <shrekloginov@gmail.com>
2023-05-01 16:36:59 +0500

    add user2

---
+++ db/migrate/20230501103108_add_author_to_question_and_answer.rb
@@ -0,0 +1,7 @@
+class AddAuthorToQuestionAndAnswer < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :questions, :author, foreign_key: { to_table: :users }
+    add_reference :answers, :author, foreign_key: { to_table: :users }
+
+  end
+end
