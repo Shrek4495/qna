c3bf892c6e800b520fd38adb98154a4c565f0f72
shrek4495 <shrekloginov@gmail.com>
2023-05-17 13:03:31 +0500

    add can

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,34 +1,34 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
-  def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-      return unless user.present?
-      can :read, :all
-    can :destroy, Question, { user_id: user.id }
-      return unless user.admin?
-      can :manage, :all
+  attr_reader :user
 
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+  def initialize(user)
+    @user = user
+    if user
+      user.admin? ? admin_abilities : user_abilities
+    else
+      guest_abilities
+    end
+  end
+
+  def admin_abilities
+    can :manage, :all
+  end
+
+  def guest_abilities
+    can :read, :all
+  end
+
+  def user_abilities
+    guest_abilities
+    can :me, User, { user_id: user.id }
+    can :create, [Question, Answer]
+    can %i[update destroy], [Question, Answer], author_id: user.id
+    can :destroy, Link, linkable: { author_id: user.id }
+
+    can :destroy, ActiveStorage::Attachment do |file|
+      user.id.eql?(file.record.author_id)
+    end
   end
 end
